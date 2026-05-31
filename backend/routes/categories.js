a19a9f82d45d5e8a318a5adaeb36b29bf9746c5f
JtaM180 <julioantamayorobalino@gmail.com>
2026-05-31 16:50:28 -0500

    Agregar POST categorias

--- backend/routes/categories.js
+++ backend/routes/categories.js
@@ -2,11 +2,20 @@ const express = require('express')
 const router = express.Router()
 const { Category } = require('../data/db')
 
-// GET todas las categorías
 router.get('/', async (req, res) => {
   try {
     const categories = await Category.find()
     res.json(categories)
+  } catch (err) {
+    res.status(500).json({ error: err.message })
+  }
+})
+
+router.post('/', async (req, res) => {
+  try {
+    const category = new Category(req.body)
+    await category.save()
+    res.status(201).json(category)
   } catch (err) {
     res.status(500).json({ error: err.message })
   }
